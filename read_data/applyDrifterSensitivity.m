61eecc85b48f8efb3fd548a510233f22994840b0
Alison Laferriere <alaferriere@ucsd.edu>
2023-06-27 10:26:01 -0400

    update sensitivity functions

--- read_data/applyDrifterSensitivity.m
+++ read_data/applyDrifterSensitivity.m
@@ -1,4 +1,6 @@
 function S = applyDrifterSensitivity(S,F,type,sensor)
+% apply drifter sensitivity to data spectrogram
+% assume spectrogram produced from data sample read using audioread with range +/-1
 % INPUTS:
 %   S:      short time fourier transform or PSD
 %           (can either be Nfreq x 1 but must also specify 'sensor', or Nfreq x 16
@@ -11,18 +13,22 @@ function S = applyDrifterSensitivity(S,F,type,sensor)
 
 Nch = size(S,3);
 
-if Nch==16
+if Nch ==16
+    keyboard
     hydro_sens = getSensitivity(F,'HTI-92WB');
     omni_sens = getSensitivity(F,'GTI-M35-300-omni');
-    directional_sens = getSensitivity(F,'GTI-M35-300-directional');
+    [directional_sens,directional_phase] = getSensitivity(F,'GTI-M35-300-directional');
     sens_dB = [hydro_sens(:).*ones(1,8) omni_sens(:) directional_sens(:).*ones(1,2) zeros(length(F),5)];
 elseif Nch==1
-    sens_dB = getSensitivity(F,sensor);
+%     [sens_dB,sens_phase_deg] = getSensitivity(F,sensor);
+    H = getSensitivity(F,sensor);
+    H = H*(2^32); % sensitivity given in uPa/count, data read +/-1 from audioread
 end
 
 switch type
     case 'mag'
-        S = S.* (10.^(-sens_dB(:)/20));
+%         S = S.* (10.^(-sens_dB(:)/20)).*exp(1i*sens_phase_deg(:)*pi/180);
+          S = S.*H.';
     case 'pow'
         S = S.* (10.^(-sens_dB(:)/10));
 end
--- read_data/getSensitivity.m
+++ read_data/getSensitivity.m
@@ -9,24 +9,45 @@ function H = getSensitivity(f,sensor)
 
 % Alison B. Laferriere
 
+Vmax=5;  %Assumed peak-to peak range of the ADC
 
 switch sensor
     case 'GTI-M35-300-directional'
         ftab =  [100 4e3 7e3 10e3 12e3 20e3];
         htab = [-192 -160 -156 -155 -155.5 -160];
+        phitab = -90*ones(size(htab));
     case 'GTI-M35-300-omni'
 %         ftab =  [100  3e3  3.9e3 5e3   6e3  9e3  10e3 12e3 20e3 200e3];
 %         htab = [-163 -164 -163   -165.5 -165 -164 -165 -163 -164 -164];
         ftab =  [100 2e3 3e3 20e3];
         htab = [-163 -163 -164 -164];
+        phitab = 0*ones(size(htab));
     case 'HTI-92WB'
         ftab = [100 50e3];
         htab = [-145 -145];
+        phitab = 0*ones(size(htab));
+    case 'VS-209-omni'
+        ftab =  [1e3 4e3 7e3 8e3 10e3 20e3];
+        htab = [-162 -163 -164 -165 -165 -165];
+        phitab = 0*ones(size(htab));
+    case 'VS-209-directional'
+        ftab =  [1e3 4.5e3 10e3 20e3];
+        htab = [-185 -171 -160 -160];
+        phitab = [-90 -90 -160 -160];
 end
 if any(f==0)
     fidx = f>=100;
-    H(fidx) = interp1(log10(ftab),htab,log10(f(fidx)),'linear','extrap');
-    H(~fidx) = htab(1);
+    HdB(fidx) = interp1(log10(ftab),htab,log10(f(fidx)),'linear','extrap');
+    HdB(~fidx) = htab(1);
+    
+    phi_deg(fidx) = interp1(log10(ftab),phitab,log10(f(fidx)),'linear','extrap');
+    phi_deg(~fidx) = phi_deg(1);
 else
-    H = interp1(log10(ftab),htab,log10(f));
+    HdB = interp1(log10(ftab),htab,log10(f));
+    phi_deg = interp1(log10(ftab),phitab,log10(f));
 end
+
+ H =  (10.^(-HdB/20)).*exp(1i*phi_deg*pi/180);  %%%Units of uPa/V
+ 
+ H=  (Vmax./(2^32))*H;  %%Units of uPa/(count);
+ 
