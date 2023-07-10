23908a5595ac9bf2a69052bbf74b1974f62d9fb6
Alison Laferriere <alaferriere@ucsd.edu>
2023-07-10 10:04:30 -0400

    updates for histogram plots, resolution of beamformer

--- helper_fcns/plotElevationHistogram.m
+++ helper_fcns/plotElevationHistogram.m
@@ -2,6 +2,7 @@ function plotElevationHistogram(t_utc,el_centers,elev_hist,el_hist_fig,title_str
 
 if exist('el_hist_fig','var')
     set(groot,'CurrentFigure',el_hist_fig)
+    set(el_hist_fig,'WindowState','Maximized')
 end
 if ~exist('title_str','var')
     title_str = [];
@@ -12,15 +13,19 @@ imagesc(t_utc,el_centers,squeeze(elev_hist./max(elev_hist,[],2)).')
 shading flat
 ylabel('Elevation, deg')
 xlabel('Time')
-set(gca,'fontweight','bold','fontsize',16);
+set(gca,'fontweight','bold','fontsize',14);
 ylim([-90 90])
 caxis([0 1])
 hold on
 datetick('x')
 
 ax2 = gca(); ax2.XTick = linspace(ax2.XTick(1),ax2.XTick(end),80);
+set(gca,'XTickLabelRotation',90)
+ax2 = gca(); ax2.XTick = linspace(ax2.XTick(1),ax2.XTick(end),80);
 datetick('x','HH:MM:SS','keepticks')
-set(gca,'fontweight','bold','fontsize',16);
+set(gca,'fontweight','bold','fontsize',14);
+set(gca,'XTickLabelRotation',90)
+set(gca,'fontweight','bold','fontsize',14);
 title(cat(2,{sprintf('%s to %s',datestr(xa(1)),datestr(xa(2)))},title_str))
 xlim(xa)
 xlabel('Elevation, deg')
