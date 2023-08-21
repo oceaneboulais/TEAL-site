ed16fc41a158910f489d9f08e3bc4836d9cfbe25
Alison Laferriere <alaferriere@ucsd.edu>
2023-08-21 14:23:35 -0400

    further updates to batch processing scripts, add script to create videoo

--- read_data/getFilesInRange.m
+++ read_data/getFilesInRange.m
@@ -12,13 +12,15 @@ end
 filenames = vertcat(filelist.name);
 
 % get the date and time in UTC from the filenames, sort them
-years = filenames(:,12:15);
-days = filenames(:,17:19);
-months = repmat('0101',[size(years,1) 1]);
-times = filenames(:,21:26);
+file_time_utc = timeFromFilename(filenames);
+% years = filenames(:,12:15);
+% days = filenames(:,17:19);
+% months = repmat('0101',[size(years,1) 1]);
+% times = filenames(:,21:26);
+% 
+% 
+% file_time_utc = datetime([years months times],'InputFormat','yyyyMMddHHmmss') + str2num(days)-1;
 
-
-file_time_utc = datetime([years months times],'InputFormat','yyyyMMddHHmmss') + str2num(days)-1;
 [file_time_utc,sortidx] = sort(file_time_utc);
 
 % file_time_posix = convertTo(file_time_utc,'posixtime');
