a19b7423981af8e62e018898a89395e194d72c16
Alison Laferriere <alaferriere@ucsd.edu>
2023-09-15 12:13:15 -0400

    add envdir

--- setUpDrifterPaths.m
+++ setUpDrifterPaths.m
@@ -1,4 +1,4 @@
-function [drivename,savedir,gitpath] = setUpDrifterPaths(use_remote,add_path)
+function [data_basedir,procdata_basedir,gitpath,envdir] = setUpDrifterPaths(use_remote,add_path)
 % set up paths depending on what system I'm using
 % drivename: base directory where all drifter data is hosted
 % savedir: base directory where all analysis products are saved
@@ -15,24 +15,27 @@ end
 
 [~,hostname] = system('hostname');
 switch hostname(1:end-1)
-    case 'Alisons-MacBook-Pro.local'
+    case {'Alisons-MacBook-Pro.local','Alisons-MBP'}
         % Alison local computer
-        gitpath = '/Users/alaferri/GIT/sio_research';
+        gitpath = '/Users/alaferri/GIT/sio_code/sio_research';
         if use_remote % use jonah
             fprintf('Using remote path to jonah on Alison''s computer\n')
-            drivename = '/Volumes/Laferriere/ONR_DRIFTER';
-            savedir = '/Volumes/Laferriere/Analysis';
+            data_basedir = '/Volumes/Laferriere/ONR_DRIFTER';
+            procdata_basedir = '/Volumes/Laferriere/Analysis';
+            envdir = '/Volumes/Laferriere/Databases';
         else % use external drive
             fprintf('Using local path to Novus on Alison''s computer\n')
-            drivename = '/Volumes/Novus/ONR_DRIFTER/';
-            savedir = '/Volumes/Novus/Analysis';          
+            data_basedir = '/Volumes/Novus/ONR_DRIFTER/';
+            procdata_basedir = '/Volumes/Novus/Analysis';     
+            envdir = '/Users/alaferri/Databases';
         end
     case 'macmussel-2.ucsd.edu'
         % this is for macmussel / Alison
         fprintf('Using remote path to Jonah on Alison''s macmussel account\n')
         gitpath = '/Volumes/Laferriere/GIT/sio_research_jonah';
-        drivename = '/Volumes/Laferriere/ONR_DRIFTER';
-        savedir = '/Volumes/Laferriere/Analysis';
+        data_basedir = '/Volumes/Laferriere/ONR_DRIFTER';
+        procdata_basedir = '/Volumes/Laferriere/Analysis';
+        envdir = '/Volumes/Laferriere/Databases';
     otherwise 
         error('set your path!')
 end
